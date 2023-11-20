e52716e74c92e49cf62e4cba497223c6ea8adc3a
NoeGRANGE <noe.grange@epitech.eu>
2023-11-20 12:42:32 +0100

    [✅] xor handle 'no block mode'

--- xor.py
+++ xor.py
@@ -3,6 +3,7 @@ def xor_message(message, key):
     byte_key = list(bytes.fromhex(key))
     xored_message = []
     length = len(byte_message)
+    key_length = len(byte_key)
     for i in range(length):
-        xored_message.append(byte_message[i] ^ byte_key[i])
+        xored_message.append(byte_message[i] ^ byte_key[i % key_length])
     print(bytes(xored_message).hex())
