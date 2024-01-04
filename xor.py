142a6f734ac793a1f70bb0e97e252a5840948b65
NoeGRANGE <noe.grange@epitech.eu>
2024-01-04 14:43:55 +0100

    [🐛] xor for smaller key

--- xor.py
+++ xor.py
@@ -3,7 +3,7 @@ def xor_message(message, key):
     byte_key = list(bytes.fromhex(key))
     xored_message = []
     key_length = len(byte_key)
-    length = min(len(byte_message), key_length)
-    for i in range(length):
+    mess_length = len(byte_message)
+    for i in range(mess_length):
         xored_message.append(byte_message[i] ^ byte_key[i % key_length])
     print(bytes(xored_message).hex())
